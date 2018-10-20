5007a93967b7f08972c24b6e36a661051c156b8e
ppjoin <38512728+ppjoin@users.noreply.github.com>
2018-10-20 11:59:48 +0800

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export HOME=/config
 sleep 10
-if [ -f "$AM" ]; then 
+if [ -z $AM ]; then 
 	exec 2>&1 /usr/bin/firefox --display=0:0
 else
 	exec 2>&1 /usr/bin/firefox --display=0:0 -p am1 https://www.alexamaster.net/Master/$AM
